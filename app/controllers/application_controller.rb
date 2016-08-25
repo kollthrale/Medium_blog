640ecf198d7b3ec2d9dc3b9ea5fe986e52e93973
christian Lisonbee <christian.lisonbee@gmail.com>
2016-08-25 16:41:50 -0600

    comments and gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+	commontator_thread_show(@commontable)
   def after_sign_in_path_for(resource)
   	blogger_path(current_user)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	validates_presence_of :title, :content
+	acts_as_commontable
+
 end

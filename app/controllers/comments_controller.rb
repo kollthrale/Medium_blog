fec1deeeb0fcd8ad6117cb34c7b2996ceba83ce7
christian Lisonbee <christian.lisonbee@gmail.com>
2016-08-25 14:24:00 -0600

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,46 @@
 class CommentsController < ApplicationController
+  
+ 
+
   def index
+  	@comments = @post.comment
   end
 
   def show
+  	@comment = @post.comment.find(params[:id])
+  end
+ 
+ 	def new
+  	@comment = Comment.new
   end
 
-  def new
-  end
 
   def edit
+  	@comment = @blogger.post.comment
   end
-end
+
+  def update
+  	if @comment.update(comment_params)
+  		redirect_to blogger_post_path(@blogger, @post)
+  	else
+  		render :edit
+  	end
+	end
+
+	def destroy 
+		@comment.destroy
+		redirect_to post_comment_path(@comment)
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:opinion, :user_id, :post_id)
+	end
+
+	def blogger 
+		@blogger = Bloger.find(params [:blogger_id])
+	end
+
+	def post 
+		@po
